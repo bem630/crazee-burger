4b80c571063ce259ea8450118f6484b99dac6dc8
bem630 <bah70495@gmail.com>
2023-11-28 19:44:04 +0100

    ✨  F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,7 +1,30 @@
+import { useState } from "react"
+
 const LoginPage = () => {
+    //state
+    const [inputValue, setInputValue] = useState("");
+
+  //comportements
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  }
+  
+  
+
+  //affichage (render)
     return ( 
         <div>
-            <h1>LoginPage</h1>
+            <h1>Bienvenue chez nous !</h1><br />
+            <h2>Connectez vous</h2>
+            <form action="submit" onSubmit={handleSubmit}>
+                <input value={inputValue} onChange={handleChange} type="text" placeholder="Entrez votre prénom..." required />
+                <button>Accédez à votre espace</button>
+            </form>
         </div>
      );
 }
