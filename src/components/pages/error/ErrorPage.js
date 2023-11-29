e191ae0c414b3027883b131835ab662ac2c6803b
bem630 <bah70495@gmail.com>
2023-11-29 13:12:46 +0100

    ✨  F02: APP > install routing system

---
+++ src/components/pages/error/ErrorPage.js
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+    return ( 
+        <div>
+            <h1>Oups Page not Faound</h1>
+            <p>Error 404</p>
+        </div>
+     );
+}
+ 
+export default ErrorPage;
